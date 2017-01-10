6e8d5b9afa6fde09feed0a1b24b587a78ea908ca
Aleksandr White <aleksandr@asdfjackal.com>
2017-01-10 14:14:57 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,49 @@
+var Discord = require("discord.js");
+var ytdl = require("ytdl-core");
+var bot = new Discord.Client();
+
+const streamOptions = { seek: 0, volume: 1 };
+
+bot.on("message", msg => {
+    if(msg.author.bot) return;
+
+    if (msg.content.startsWith("!echo")) {
+      msg.channel.sendMessage(msg.content.slice(5).trim())
+    }
+
+    if (msg.content.startsWith("!dc")) {
+      bot.voiceConnections.array().forEach( connection => {
+        connection.disconnect();
+      });
+    }
+
+    if (msg.content.startsWith("!play")) {
+      var url = msg.content.slice(5).trim();
+
+      var voiceChannel = null;
+
+      for (var channel of msg.channel.guild.channels.array()) {
+  			// If the channel is a voice channel, ...
+        console.log(channel.type);
+  			if (channel.type === 'voice') {
+          console.log(channel.type);
+  				// ... and join the channel
+          // channel.join()
+          //   .then(connection => console.log('Connected!'))
+          //   .catch(console.error);
+          channel.join().then(connection => {
+            const stream = ytdl(url, {filter : 'audioonly'});
+            const dispatcher = connection.playStream(stream, streamOptions);
+          }).catch(console.error);
+
+  				break;
+  			}
+  		}
+    }
+});
+
+bot.on('ready', () => {
+  console.log('I am ready!');
+});
+
+bot.login(process.env.BOT_TOKEN);
