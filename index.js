1bab4241e0a7ea464506c9719ecb35fa0cfb4fe5
Aleksandr White <aleksandr@asdfjackal.com>
2017-01-10 19:33:30 -0500

    Added group filtering to music controls

--- index.js
+++ index.js
@@ -5,41 +5,49 @@ var bot = new Discord.Client();
 const streamOptions = { seek: 0, volume: 1 };
 
 bot.on("message", msg => {
-    if(msg.author.bot) return;
+    if (msg.author.bot) return;
+
+    if !(msg.content.startsWith("!")) return;
 
     if (msg.content.startsWith("!echo")) {
       msg.channel.sendMessage(msg.content.slice(5).trim())
     }
 
-    if (msg.content.startsWith("!dc")) {
-      bot.voiceConnections.array().forEach( connection => {
-        connection.disconnect();
-      });
-    }
+    userIsDJ = false;
+    members = msg.guild.roles.filter('name',"DJ").members;
+    members.forEach(member => {
+      if (member.user.id === msg.author.user.id) {
+        userIsDJ = true;
+      }
+    });
 
-    if (msg.content.startsWith("!play")) {
-      var url = msg.content.slice(5).trim();
+    if (userIsDJ){
+      if (msg.content.startsWith("!stop")) {
+        bot.voiceConnections.array().forEach( connection => {
+          connection.disconnect();
+        });
+      }
+      if (msg.content.startsWith("!play")) {
+        var url = msg.content.slice(5).trim();
 
-      var voiceChannel = null;
+        var voiceChannel = null;
 
-      for (var channel of msg.channel.guild.channels.array()) {
-  			// If the channel is a voice channel, ...
-        console.log(channel.type);
-  			if (channel.type === 'voice') {
+        for (var channel of msg.channel.guild.channels.array()) {
+    			// If the channel is a voice channel, ...
           console.log(channel.type);
-  				// ... and join the channel
-          // channel.join()
-          //   .then(connection => console.log('Connected!'))
-          //   .catch(console.error);
-          channel.join().then(connection => {
-            const stream = ytdl(url, {filter : 'audioonly'});
-            const dispatcher = connection.playStream(stream, streamOptions);
-          }).catch(console.error);
+    			if (channel.type === 'voice') {
+            console.log(channel.type);
+            channel.join().then(connection => {
+              const stream = ytdl(url, {filter : 'audioonly', quality: 'lowest'});
+              const dispatcher = connection.playStream(stream, streamOptions);
+            }).catch(console.error);
 
-  				break;
-  			}
-  		}
+    				break;
+    			}
+    		}
+      }
     }
+
 });
 
 bot.on('ready', () => {
